698799828d366e13b1b1377c472ff0a1b3dd866c
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-09-30 14:56:07 +0600

    load filtered data from db for a specific user

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ client.connect(err => {
         console.log(newBooking);
     })
 
+    app.get('/bookings', (req, res) => {
+        // console.log(req.query.email);
+        bookings.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
 });
 
 
