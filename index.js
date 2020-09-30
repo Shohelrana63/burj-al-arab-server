729954252e112ba822fafd83fcdb8e3034390ec9
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-09-30 13:35:08 +0600

    create post in mongoDB & successful get data

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 const port = 5000
 
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
 
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://arabian:ArabianHorse60@cluster0.sqmks.mongodb.net/burjAlArab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("burjAlArab").collection("bookings");
-    console.log('db connect successful');
-    client.close();
+    const bookings = client.db("burjAlArab").collection("bookings");
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+        console.log(newBooking);
+    })
+
 });
 
 
